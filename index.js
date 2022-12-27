350283b630323ddf4d0b581eb784e194f775037f
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-12-27 14:44:25 -0600

    Added buttons functionality, added interactive cookie consent modal , finished app

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
-const modalForm = document.getElementById('modal-form')
+const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -9,34 +11,39 @@ setTimeout(function(){
 
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
-})
+}) 
 
-modalForm.addEventListener('submit', (e) => {
+declineBtn.addEventListener('mouseenter', function(){
+    modalChoiceBtns.classList.toggle('modal-btns-reverse')
+}) 
+
+consentForm.addEventListener('submit', function(e){
     e.preventDefault()
-    console.log("form submitted")
+    
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
     
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
-        <p id="uploadText">
-            Uploading your data to the dark web...
-        </p>
-    </div>
-`
-
-    setTimeout(()=>{
-        document.getElementById('uploadText').textContent = `Making the 
-        sale...`
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>` 
+    
+    setTimeout(function(){
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+    
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    `
+    modalCloseBtn.disabled = false
     }, 3000)
-})
-
-
-/*
-Challenge:
-1. Take control of the "modal-text" element. 
-2. Make it so that when a user clicks on 
-   the accept button, the HTML string below
-   is inserted into the modal-text div.
-
-
-*/ 
+  
+}) 
