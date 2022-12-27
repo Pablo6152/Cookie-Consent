f62a48a9d6d163b35c7db472283918d5fe6341ac
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-12-27 00:06:30 -0600

    Added form

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
-
-modalCloseBtn.addEventListener('click', () => {
-    modal.style.display = "none"
-})
+const modalForm = document.getElementById('modal-form')
+const modalText = document.getElementById('modal-text')
 
 setTimeout(function(){
-    modal.style.display = "inline"
+    modal.style.display = 'inline'
 }, 1500)
+
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
+
+modalForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log("form submitted")
+    
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>
+`
+
+    setTimeout(()=>{
+        document.getElementById('uploadText').textContent = `Making the 
+        sale...`
+    }, 3000)
+})
+
+
+/*
+Challenge:
+1. Take control of the "modal-text" element. 
+2. Make it so that when a user clicks on 
+   the accept button, the HTML string below
+   is inserted into the modal-text div.
+
+
+*/ 
